843b11ec5bf429f67e12a8e4fe0d4c1f24dafcaf
Iruyan_Zak <yuri.kazuna@gmail.com>
2017-11-09 00:08:27 +0900

    2回目用

--- app.py
+++ app.py
@@ -37,61 +37,61 @@ def internal_error():
 
 @app.route('/')
 def index():
-    print("ルートポイント")
+    # print("ルートポイント")
     return render_index()
 
 
-# @app.route('/posted')
+@app.route('/posted')
 def posted():
     try:
         n = int(request.args.get('n'))
     except Exception as e:
-        print(e)
+        # print(e)
         return error("自然数は1以上1000000以下で入力してください。")
 
     try:
         twitter = request.args.get('twitter').strip().lower()
 
         if not twitter:
-            print("空フォームなど")
+            # print("空フォームなど")
             return error("フォームの内容を正しく確認できませんでした。")
     except Exception as e:
-        print(e)
+        # print(e)
         return internal_error()
 
-    print(twitter, n)
+    # print(twitter, n)
 
     re_result = username_regex.search(twitter)
 
     if re_result:
-        print("Twitter異常")
+        # print("Twitter異常")
         return error("Twitter IDに不明な文字が検出されました。")
 
     if n < 1 or 1000000 < n:
-        print("自然数がダメ")
+        # print("自然数がダメ")
         return error("自然数は1以上1000000以下で入力してください。")
 
     success_msg = f"@{twitter}さんの投稿: "
 
     try:
-        print("クエリ投げるよ")
+        # print("クエリ投げるよ")
         post = Post.query.filter_by(twitter=twitter).first()
 
         if not post:
-            print("レコードなし")
+            # print("レコードなし")
             db.session.add(Post(twitter, n))
             success_msg += f"n={n}を受け付けました。"
         else:
-            print("レコードあり")
+            # print("レコードあり")
             post.n = n
             success_msg += f"n={n}に更新しました。"
         db.session.commit()
-        print(Post.query.filter_by(twitter=twitter).first())
+        # print(Post.query.filter_by(twitter=twitter).first())
     except Exception as e:
-        print(e)
+        # print(e)
         return internal_error()
 
-    print("投稿完了")
+    # print("投稿完了")
     return render_index(text_class='text-success', text_contents=success_msg)
 
 
